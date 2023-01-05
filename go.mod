5d8ac59eb01b2704fb33babc47cc6ce71c39a8c3
riyan-eng <fbriyant11@gmail.com>
2023-01-05 08:07:18 +0700

    upgrade module sayhello

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/riyan-eng/go-app-say-hello
 
 go 1.19
 
-require github.com/riyan-eng/go-say-hello v1.5.0
+require (
+	github.com/riyan-eng/go-say-hello/v2 v2.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	gosayhello "github.com/riyan-eng/go-say-hello"
+	gosayhello "github.com/riyan-eng/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(gosayhello.SayHello())
+	fmt.Println(gosayhello.SayHello("riyan"))
 }
